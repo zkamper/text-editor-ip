655d16608425366ed8a07daf68929792c038f16b
Insomnia2304 <sebastian.barbarasa@gmail.com>
2022-11-25 17:58:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,21 +29,21 @@ int main()
     winheight = 720;
     initwindow(winlenght, winheight, "Notepad^2");
     setbkcolor(COLOR(221,234,235));
-    settextstyle(SANS_SERIF_FONT,0,0);
+    settextstyle(DEFAULT_FONT,0,0);
     cleardevice();
-    drawIcons();
+    ///drawIcons();
     setcolor(BLACK);
     curr=getch();
     while (curr!=27) ///escape
           {
-           if (curr==13) {line_index++; y=y+textheight("M"); x=0;}
+           if (curr==13) {line_index++; y=y+textheight("Y")-1; x=0;}
            else
                {
                 lines[line_index][col_index[line_index]]=curr;
                 bgiout<<lines[line_index][col_index[line_index]];
                 col_index[line_index]++;
                 outstreamxy(x,y);
-                x=x+textwidth("M")-20;
+                x=x+textwidth("Y")+2;
                }
            curr=getch();
           }
