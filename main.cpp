1588ff2cd58e806cfce00dd895ff9647294a2285
Insomnia2304 <sebastian.barbarasa@gmail.com>
2022-11-25 17:42:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <winbgim.h>
 #include <graphics.h>
+#define LMAX 100
+#define COLMAX 1024
 using namespace std;
 
 int winlenght, winheight;
 
-char lines[100][1024];
-char c;
+char lines[LMAX][COLMAX];
+int col_index[LMAX];
+int line_index=0;
+char curr;
+
 int x, y;
 
 int main()
@@ -15,18 +20,24 @@ int main()
     winheight = 720;
     initwindow(winlenght, winheight, "Notepad^2");
     setbkcolor(COLOR(221,234,235));
+    settextstyle(SANS_SERIF_FONT,0,0);
     cleardevice();
 
     setcolor(BLACK);
-    c=getch();
-    while (c!=27) ///escape
+    curr=getch();
+    while (curr!=27) ///escape
           {
-
-           line(x,y,x+50,y+50);
-           x=y=x+10;
-           c=getch();
+           if (curr==13) {line_index++; y=y+textheight("M"); x=0;}
+           else
+               {
+                lines[line_index][col_index[line_index]]=curr;
+                bgiout<<lines[line_index][col_index[line_index]];
+                col_index[line_index]++;
+                outstreamxy(x,y);
+                x=x+textwidth("M")-20;
+               }
+           curr=getch();
           }
-
 
 
     closegraph();
