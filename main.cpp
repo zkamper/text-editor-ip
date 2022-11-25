3a5fec8cc2fd8bbc49e8bb937df2221f05fa41e8
zkamper <ionut.calin645@gmail.com>
2022-11-25 17:34:41 +0200

    Icons

--- main.cpp
+++ main.cpp
@@ -9,6 +9,15 @@ char lines[100][1024];
 char c;
 int x, y;
 
+void drawIcons()
+{
+  readimagefile("icons\\save_icon.gif",5,5,36,36);
+  readimagefile("icons\\copy_icon.gif",46,5,77,36);
+  readimagefile("icons\\paste_icon.gif",87,5,118,36);
+}
+
+
+
 int main()
 {   ///initializare fereastra
     winlenght = 1280;
@@ -16,7 +25,7 @@ int main()
     initwindow(winlenght, winheight, "Notepad^2");
     setbkcolor(COLOR(221,234,235));
     cleardevice();
-
+    drawIcons();
     setcolor(BLACK);
     c=getch();
     while (c!=27) ///escape
