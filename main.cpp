ab9b8578e968eb3db6ae894c3d15cb34c9789f7a
Insomnia2304 <sebastian.barbarasa@gmail.com>
2022-11-25 16:52:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <winbgim.h>
 #include <graphics.h>
 using namespace std;
 
 int winlenght, winheight;
+char lines[100][1024];
+char d;
 
 int main()
 {
@@ -10,12 +13,15 @@ int main()
  winheight=720;
  initwindow(winlenght, winheight, "Notepad^2");
 
+ d=getch();
+ cout<<d;
 
  int x = 400, y = 300, r;
  line(x,y,x+100,y+100);
 
 
- getch();
- closegraph();
+
+
+ getch(); closegraph();
  return 0;
 }
