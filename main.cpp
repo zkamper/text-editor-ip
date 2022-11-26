d4d9a5877b254a1f84a43520eb7770479adfa155
Insomnia2304 <sebastian.barbarasa@gmail.com>
2022-11-26 15:21:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,11 @@
 #include <winbgim.h>
 #include <graphics.h>
 #include "display.h"
-#define LMAX 100
-#define COLMAX 1024
+#define NMAX 10000
 using namespace std;
 
-int winlenght, winheight;
-
-char lines[LMAX][COLMAX];
-int col_index[LMAX];
-int line_index=0;
-char curr;
-
+double lengthError;
 Button saveButton,copyButton,pasteButton,fontButton;
-
-int x, y;
-
 
 void drawIcons()
 {
@@ -24,30 +14,30 @@ void drawIcons()
     copyButton.bkcolor = COLOR(177,187,188);
     pasteButton.bkcolor = COLOR(177,187,188);
     fontButton.bkcolor =  COLOR(177,187,188);
-    
+
     saveButton.icon = "icons\\save_icon.gif";
     copyButton.icon = "icons\\copy_icon.gif";
     pasteButton.icon = "icons\\paste_icon.gif";
-    
+
     saveButton.text = "Save";
     copyButton.text = "Copy";
     pasteButton.text = "Paste";
     fontButton.text = "Font";
-    
+
     saveButton.iconWidth = pasteButton.iconWidth = copyButton.iconWidth = 32;
     saveButton.iconHeight = pasteButton.iconHeight = copyButton.iconHeight =  32;
-    
+
     int offset = 5;
     saveButton.buttonWidth = pasteButton.buttonWidth = copyButton.buttonWidth = fontButton.buttonWidth = 125;
     saveButton.buttonHeight = pasteButton.buttonHeight = copyButton.buttonHeight = fontButton.buttonHeight = 40;
-    
+
     saveButton.b.x = offset;
     copyButton.b.x = saveButton.b.x+saveButton.buttonWidth+offset;
     pasteButton.b.x = copyButton.b.x+copyButton.buttonWidth+offset;
     fontButton.b.x = pasteButton.b.x + pasteButton.buttonWidth+offset;
-    
+
     saveButton.b.y = copyButton.b.y = pasteButton.b.y = fontButton.b.y = offset;
-    
+
     drawButton(saveButton);
     drawButton(copyButton);
     drawButton(pasteButton);
@@ -76,54 +66,62 @@ void setTextFont(double &lengthError)
 
 void getButton(int x, int y)
 {
-    
+
 }
 
+int winLength, winHeight;
 void windowsInit()
 {
  ///initializare fereastra
-    winlenght = 1280;
-    winheight = 720;
-    initwindow(winlenght, winheight, "Notepad^2");
+    winLength = 1280;
+    winHeight = 720;
+    initwindow(winLength, winHeight, "Notepad^2");
     setbkcolor(COLOR(221,234,235));
     cleardevice();
     drawIcons();
     registermousehandler(WM_RBUTTONDOWN,getButton);
 }
 
-int main()
+int x, y;
+char text[NMAX];
+int lgtext;
+char curr;
+
+void getText()
 {
-    windowsInit();
-    y = saveButton.buttonHeight+10;
     double offsetHeight;
-    double offsetLenght;
-    double lengthError;
+    double offsetLength;
     setTextFont(lengthError);
+    y = saveButton.buttonHeight+10;
 
     curr=getch();
     while (curr!=27) ///escape
-          {
-           if (curr==13)
-              {
-               line_index++;
-               offsetHeight=textheight(&curr);
-               y=y+offsetHeight;
-               x=0;
-              }
-           else
-               {
-                lines[line_index][col_index[line_index]]=curr;
-                ///afisez
-                bgiout<<lines[line_index][col_index[line_index]];
-                outstreamxy(x,y);
-                offsetLenght=textwidth(&curr);
-                x=x+lengthError*offsetLenght;
-                col_index[line_index]++;
-               }
-           curr=getch();
-          }
+    {
+        if (curr==13)
+        {
+            offsetHeight=textheight(&curr);
+            y=y+offsetHeight;
+            x=0;
+        }
+        else
+        {
 
+            ///afisez
+            bgiout<<curr;
+            outstreamxy(x,y);
+            offsetLength=textwidth(&curr);
+            x=x+lengthError*offsetLength;
+        }
+        text[lgtext++]=curr;
+        curr=getch();
+    }
 
+}
+
+int main()
+{
+    windowsInit();
+    getText();
     closegraph();
     return 0;
 }
