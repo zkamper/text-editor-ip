00c296e728bf5dc29056bc0b846b603b15995709
zkamper <ionut.calin645@gmail.com>
2022-11-28 10:02:26 +0200

    dadaw

--- main.cpp
+++ main.cpp
@@ -6,21 +6,25 @@
 using namespace std;
 
 double lengthError;
-Button saveButton,copyButton,pasteButton,fontButton;
+Button saveButton, copyButton, pasteButton, fontButton;
 
-struct {int x, y, length; char c;} text[NMAX];
+struct
+{
+    int x, y, length;
+    char c;
+} text[NMAX];
 
 int lgtext;
 
-void getButtonClick(int x,int y);
+void getButtonClick(int x, int y);
 void setPosChar(char curr);
 
 void drawIcons()
 {
-    saveButton.bkcolor = COLOR(177,187,188);
-    copyButton.bkcolor = COLOR(177,187,188);
-    pasteButton.bkcolor = COLOR(177,187,188);
-    fontButton.bkcolor =  COLOR(177,187,188);
+    saveButton.bkcolor = COLOR(177, 187, 188);
+    copyButton.bkcolor = COLOR(177, 187, 188);
+    pasteButton.bkcolor = COLOR(177, 187, 188);
+    fontButton.bkcolor = COLOR(177, 187, 188);
 
     saveButton.icon = "icons\\save_icon.gif";
     copyButton.icon = "icons\\copy_icon.gif";
@@ -32,16 +36,16 @@ void drawIcons()
     fontButton.text = "Font";
 
     saveButton.iconWidth = pasteButton.iconWidth = copyButton.iconWidth = 32;
-    saveButton.iconHeight = pasteButton.iconHeight = copyButton.iconHeight =  32;
+    saveButton.iconHeight = pasteButton.iconHeight = copyButton.iconHeight = 32;
 
     int offset = 5;
     saveButton.buttonWidth = pasteButton.buttonWidth = copyButton.buttonWidth = fontButton.buttonWidth = 125;
     saveButton.buttonHeight = pasteButton.buttonHeight = copyButton.buttonHeight = fontButton.buttonHeight = 40;
 
     saveButton.b.x = offset;
-    copyButton.b.x = saveButton.b.x+saveButton.buttonWidth+offset;
-    pasteButton.b.x = copyButton.b.x+copyButton.buttonWidth+offset;
-    fontButton.b.x = pasteButton.b.x + pasteButton.buttonWidth+offset;
+    copyButton.b.x = saveButton.b.x + saveButton.buttonWidth + offset;
+    pasteButton.b.x = copyButton.b.x + copyButton.buttonWidth + offset;
+    fontButton.b.x = pasteButton.b.x + pasteButton.buttonWidth + offset;
 
     saveButton.b.y = copyButton.b.y = pasteButton.b.y = fontButton.b.y = offset;
 
@@ -50,41 +54,77 @@ void drawIcons()
     drawButton(pasteButton);
     drawButton(fontButton);
 
-    registermousehandler(WM_LBUTTONDOWN,getButtonClick);
+    registermousehandler(WM_LBUTTONDOWN, getButtonClick);
 }
 
-int font=8; /// 8 - font recomandat, fara niciun offset
+int font = 8; /// 8 - font recomandat, fara niciun offset
 void setTextFont()
-{lengthError=1;
- switch (font)
- {
-  case 1:  settextstyle(TRIPLEX_FONT,0,0);     lengthError=1.3; break;
-  case 2:  settextstyle(SMALL_FONT,0,10);      lengthError=1;   break;
-  case 3:  settextstyle(SANS_SERIF_FONT,0,10); lengthError=1;   break;
-  case 4:  settextstyle(GOTHIC_FONT,0,0);      lengthError=1.2; break;
-  case 5:  settextstyle(SCRIPT_FONT,0,0);      lengthError=1;   break;
-  case 6:  settextstyle(SIMPLEX_FONT,0,0);     lengthError=1.3; break;
-  case 7:  settextstyle(TRIPLEX_SCR_FONT,0,0); lengthError=1;   break;
-  case 8:  settextstyle(COMPLEX_FONT,0,20);     lengthError=1;   break;
-  case 9:  settextstyle(EUROPEAN_FONT,0,0);    lengthError=1.3; break;
-  case 10: settextstyle(BOLD_FONT,0,0);        lengthError=1.1; break;
-  default: settextstyle(DEFAULT_FONT,0,0);     lengthError=1.2;   break;
- }
- setcolor(BLACK);
+{
+    lengthError = 1;
+    switch (font)
+    {
+    case 1:
+        settextstyle(TRIPLEX_FONT, 0, 0);
+        lengthError = 1.3;
+        break;
+    case 2:
+        settextstyle(SMALL_FONT, 0, 10);
+        lengthError = 1;
+        break;
+    case 3:
+        settextstyle(SANS_SERIF_FONT, 0, 10);
+        lengthError = 1;
+        break;
+    case 4:
+        settextstyle(GOTHIC_FONT, 0, 0);
+        lengthError = 1.2;
+        break;
+    case 5:
+        settextstyle(SCRIPT_FONT, 0, 0);
+        lengthError = 1;
+        break;
+    case 6:
+        settextstyle(SIMPLEX_FONT, 0, 0);
+        lengthError = 1.3;
+        break;
+    case 7:
+        settextstyle(TRIPLEX_SCR_FONT, 0, 0);
+        lengthError = 1;
+        break;
+    case 8:
+        settextstyle(COMPLEX_FONT, 0, 20);
+        lengthError = 1;
+        break;
+    case 9:
+        settextstyle(EUROPEAN_FONT, 0, 0);
+        lengthError = 1.3;
+        break;
+    case 10:
+        settextstyle(BOLD_FONT, 0, 0);
+        lengthError = 1.1;
+        break;
+    default:
+        settextstyle(DEFAULT_FONT, 0, 0);
+        lengthError = 1.2;
+        break;
+    }
+    setcolor(BLACK);
 }
 
 void getButtonClick(int x, int y)
 {
-    Button b[] = {copyButton,saveButton,pasteButton,fontButton};
+    Button b[] = {copyButton, saveButton, pasteButton, fontButton};
     int buttCount = 4;
-    for(int i = 0 ; i < buttCount;i++)
+    for (int i = 0; i < buttCount; i++)
     {
-        if(b[i].b.x <= x && x<=b[i].b.x+b[i].buttonWidth && b[i].b.y <= y && y<=b[i].b.y+b[i].buttonHeight)
-            cout<<b[i].text<<" ";
-        if(strcmp(b[i].text,"Font")==0)
+        if (b[i].b.x <= x && x <= b[i].b.x + b[i].buttonWidth && b[i].b.y <= y && y <= b[i].b.y + b[i].buttonHeight)
         {
-            font=(font+1)%11;
-            setTextFont();
+            cout << b[i].text << " ";
+            if (strcmp(b[i].text, "Font") == 0)
+            {
+                font = (font + 1) % 11;
+                setTextFont();
+            }
         }
     }
 }
@@ -92,108 +132,115 @@ void getButtonClick(int x, int y)
 int winLength, winHeight;
 void windowsInit()
 {
- ///initializare fereastra
+    /// initializare fereastra
     winLength = 1280;
     winHeight = 720;
     initwindow(winLength, winHeight, "Notepad^2");
-    setbkcolor(COLOR(221,234,235));
+    setbkcolor(COLOR(221, 234, 235));
     cleardevice();
     drawIcons();
 }
 
-
-
 double offsetHeight, offsetLength;
-double currWordLength=0;
-int currWordStart=0;
-int x=8, y;
+double currWordLength = 0;
+int currWordStart = 0;
+int x = 8, y;
 
 void write(int left, int right)
-{setTextFont();
- for (int i=left; i<=right; i++)
-      if (text[i].c!=32 && text[i].c!=13)
-         {
-          bgiout<<text[i].c;
-          outstreamxy(text[i].x,text[i].y);
-         }
+{
+    setTextFont();
+    for (int i = left; i <= right; i++)
+        if (text[i].c != 32 && text[i].c != 13)
+        {
+            bgiout << text[i].c;
+            outstreamxy(text[i].x, text[i].y);
+        }
 }
 
 void setPosChar(char curr)
-{setTextFont();
- if(curr == 9)  ///9 = TAB aka indentare
- text[lgtext].length=lengthError*textwidth("    ");
- else
- text[lgtext].length=lengthError*textwidth(&curr);
- offsetHeight=textheight(&curr);
- if (curr==13)
+{
+    setTextFont();
+    if (curr == 9) /// 9 = TAB aka indentare
+        text[lgtext].length = lengthError * textwidth("    ");
+    else
+        text[lgtext].length = lengthError * textwidth(&curr);
+    offsetHeight = textheight(&curr);
+    if (curr == 13)
     {
-     y+=offsetHeight; x=8;
-     currWordStart=currWordLength=0;
-     return;
+        y += offsetHeight;
+        x = 8;
+        currWordStart = currWordLength = 0;
+        return;
     }
- if (curr==32 || curr==9) ///TAB pune 4 spatii, deci cred ca ar trebui inclus si el aici
+    if (curr == 32 || curr == 9) /// TAB pune 4 spatii, deci cred ca ar trebui inclus si el aici
     {
-     x+=text[lgtext].length;
-     currWordLength=0;
-     currWordStart=lgtext+1;
-     return;
+        x += text[lgtext].length;
+        currWordLength = 0;
+        currWordStart = lgtext + 1;
+        return;
     }
- if (x+text[lgtext].length+8<=winLength)
+    if (x + text[lgtext].length + 8 <= winLength)
     {
-     bgiout<<curr;
-     outstreamxy(x,y);
+        bgiout << curr;
+        outstreamxy(x, y);
 
-     text[lgtext].x=x; text[lgtext].y=y;
-     x+=text[lgtext].length;
-     currWordLength+=text[lgtext].length;
-     return;
+        text[lgtext].x = x;
+        text[lgtext].y = y;
+        x += text[lgtext].length;
+        currWordLength += text[lgtext].length;
+        return;
     }
- if (currWordLength+text[lgtext].length+16>winLength)
+    if (currWordLength + text[lgtext].length + 16 > winLength)
     {
-     y=y+offsetHeight; x=8;
+        y = y + offsetHeight;
+        x = 8;
 
-     bgiout<<curr;
-     outstreamxy(x,y);
+        bgiout << curr;
+        outstreamxy(x, y);
 
-     text[lgtext].x=x; text[lgtext].y=y;
-     x+=text[lgtext].length;
-     currWordLength=text[lgtext].length;
-     currWordStart=lgtext;
-     return;
+        text[lgtext].x = x;
+        text[lgtext].y = y;
+        x += text[lgtext].length;
+        currWordLength = text[lgtext].length;
+        currWordStart = lgtext;
+        return;
     }
- else
-     {
-      setfillstyle(0,COLOR(221,234,235));
-      bar(0,saveButton.buttonHeight+10,winLength,winHeight);
-      write(0,currWordStart-1);
-      y=y+offsetHeight; x=8;
-      for (int i=currWordStart; i<=lgtext; i++)
-          {
-           text[i].y=y; text[i].x=x; x+=text[i].length; ///bug
-          }
-      write(currWordStart,lgtext);
-      currWordLength+=text[lgtext].length;
-      return;
-     }
+    else
+    {
+        setfillstyle(0, COLOR(221, 234, 235));
+        bar(0, saveButton.buttonHeight + 10, winLength, winHeight);
+        write(0, currWordStart - 1);
+        y = y + offsetHeight;
+        x = 8;
+        for (int i = currWordStart; i <= lgtext; i++)
+        {
+            text[i].y = y;
+            text[i].x = x;
+            x += text[i].length; /// bug
+        }
+        write(currWordStart, lgtext);
+        currWordLength += text[lgtext].length;
+        return;
+    }
 }
 
 void readText()
 {
     char curr;
-    curr=getch();
-    while (curr!=27) ///escape
-    {   text[lgtext].c=curr;
+    curr = getch();
+    while (curr != 27) /// escape
+    {
+        text[lgtext].c = curr;
         setPosChar(curr);
         lgtext++;
-        curr=getch();
+        curr = getch();
     }
-
 }
 
 int main()
 {
     windowsInit();
-    y=saveButton.buttonHeight+10;
+    y = saveButton.buttonHeight + 10;
     readText();
     closegraph();
     return 0;
